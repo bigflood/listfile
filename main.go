05e0d07dfab3bba84482a9a7a2a4b548adc3e128
표태수 <pyots@nexon.co.kr>
2018-06-29 09:37:46 +0900

    flags: -r reverse order, -v value type

--- main.go
+++ main.go
@@ -1,31 +1,63 @@
 package main
 
+//go:generate stringer -type=Value
+
 import (
+	"errors"
 	"flag"
 	"fmt"
-	"github.com/dustin/go-humanize"
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
+
+	"github.com/dustin/go-humanize"
+)
+
+type Value int
+
+const (
+	valueStart Value = iota
+	SizeValue
+	DateValue
+	NameValue
+	valueLast
 )
 
 func main() {
 	limitCount := flag.Int("n", 10, "file list size")
+	reverseOrder := flag.Bool("r", false, "sort reverse order")
 	ignoreHiddenFiles := flag.Bool("f", false, "ignore hidden files")
+	fileValue := flag.String("v", "size", "value type (size, date, name)")
 	flag.Parse()
+
+	valueType, err := strToValue(*fileValue)
+	if err != nil {
+		fmt.Println(err)
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
 
 	obj := new(lister)
 	obj.count = *limitCount
 	obj.ignoreHiddenFiles = *ignoreHiddenFiles
+	obj.reverseOrder = *reverseOrder
+	obj.valueType = valueType
+	obj.init()
 
 	args := flag.Args()
 
+	if len(args) == 0 {
+		args = []string{"."}
+	}
+
 	for _, n := range args {
-		obj.walk(n)
+		absPath, _ := filepath.Abs(n)
+		obj.walk(absPath)
 	}
 
 	for _, f := range obj.files {
-		fmt.Printf("%6v %s \n", humanize.Bytes(uint64(f.info.Size())), f.path)
+		obj.printFile(f)
 	}
 
 	fmt.Printf("%v files (%v) \n", humanize.Comma(int64(obj.numFiles)), humanize.Bytes(uint64(obj.sumSize)))
@@ -41,9 +73,14 @@ type lister struct {
 
 	count             int
 	ignoreHiddenFiles bool
+	reverseOrder      bool
+	valueType         Value
 
 	numFiles int
 	sumSize  int64
+
+	compareFile func(a, b file) int
+	printFile   func(f file)
 }
 
 func (obj *lister) walk(path string) error {
@@ -92,7 +129,7 @@ func (obj *lister) addFile(path string, info os.FileInfo) error {
 
 	n := len(obj.files)
 	i := sort.Search(n, func(i int) bool {
-		return obj.compareFile(f, obj.files[i])
+		return obj.compareFile(f, obj.files[i]) < 0
 	})
 
 	obj.files = append(obj.files, f)
@@ -106,6 +143,52 @@ func (obj *lister) addFile(path string, info os.FileInfo) error {
 	return nil
 }
 
-func (obj *lister) compareFile(a, b file) bool {
-	return a.info.Size() > b.info.Size()
+func (obj *lister) init() {
+	switch obj.valueType {
+	case SizeValue:
+		obj.compareFile = func(a, b file) int {
+			cmp := a.info.Size() - b.info.Size()
+			return -int(cmp)
+		}
+
+		obj.printFile = func(f file) {
+			fmt.Printf("%6v %s \n", humanize.Bytes(uint64(f.info.Size())), f.path)
+		}
+	case DateValue:
+		obj.compareFile = func(a, b file) int {
+			cmp := a.info.ModTime().Sub(b.info.ModTime())
+			return -int(cmp)
+		}
+
+		obj.printFile = func(f file) {
+			fmt.Printf("%6v  %s \n", f.info.ModTime().Format("2006-01-02 15:04:05 MST"), f.path)
+		}
+	case NameValue:
+		obj.compareFile = func(a, b file) int {
+			cmp := strings.Compare(a.info.Name(), b.info.Name())
+			return cmp
+		}
+
+		obj.printFile = func(f file) {
+			fmt.Printf("%20v %s \n", f.info.Name(), f.path)
+		}
+	}
+
+	if obj.reverseOrder {
+		originalFunc := obj.compareFile
+		obj.compareFile = func(a, b file) int {
+			return -originalFunc(a, b)
+		}
+	}
+}
+
+func strToValue(s string) (Value, error) {
+	s += "value"
+
+	for v := valueStart + 1; v < valueLast; v++ {
+		if strings.ToLower(v.String()) == s {
+			return v, nil
+		}
+	}
+	return 0, errors.New("unknown value: " + s)
 }
